b251a39fed1b8dd163d212d81fdb359b02b683b7
stevefunnylocks <117920746+stevefunnylocks@users.noreply.github.com>
2022-12-05 17:00:27 +0900

    Delete navbar.js

--- navbar.js
+++
@@ -1,7 +0,0 @@
-// if sub-menu button is clicked.
-const sub_menu = document.querySelector(".sub-menu");
-const container = document.querySelector(".container");
-
-sub_menu.addEventListener("click", () => {
-    container.classList.toggle("active");
-});
