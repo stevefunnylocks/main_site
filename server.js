46bc565e31e469f376a4347c6e70624bd4ea4e9d
stevefunnylocks <117920746+stevefunnylocks@users.noreply.github.com>
2022-12-05 17:43:58 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/public'));
 app.use('/build/', express.static(path.join(__dirname, './build')));
-app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
+app.use('/jsm/', express.static(path.join(__dirname, './three/examples/jsm')));
 
 app.listen(4000, ()=>{
     console.log('visit http://www.funnylocks.com');
