4b2defbf03bdf40a7da4cda194ffcd65ec4d7f65
stevefunnylocks <117920746+stevefunnylocks@users.noreply.github.com>
2022-12-05 17:01:42 +0900

    Delete user directory

--- user/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class UserModel(AbstractUser): 
-    class Meta:
-        db_table = 'user'
-
-    nickname = models.CharField(max_length=40, blank=True)
