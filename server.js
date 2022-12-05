66547cb4c03a0c806991cf7dd8d6e3afd4f40fde
stevefunnylocks <117920746+stevefunnylocks@users.noreply.github.com>
2022-12-05 17:27:30 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(__dirname + '/public'));
-app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
+app.use('/build/', express.static(path.join(__dirname, './build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
 app.listen(4000, ()=>{
